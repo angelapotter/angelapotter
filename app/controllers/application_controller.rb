d00b4e433922f93be8038d72fc21d532038a5c90
Angela Potter <angelacbpotter@gmail.com>
2017-07-16 15:38:52 -0700

    Update case study pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
   helper_method :body_css_class
   helper_method :portfolio_items
+  helper_method :portfolio_items_with_case_studies
   helper_method :social_urls
   helper_method :contact_info
   helper_method :current_job
@@ -14,8 +15,20 @@ class ApplicationController < ActionController::Base
     @portfolio_items ||= PortfolioItems.all
   end
 
+  def portfolio_items_with_case_studies
+    portfolio_items_with_case_studies = []
+    @portfolio_items.each_with_index do |item, index|
+      portfolio_items_with_case_studies << item if item.show_case_study?
+    end
+    portfolio_items_with_case_studies
+  end
+
   def body_css_class
-    "#{ params[ :action ].gsub( '_', '-' ) } #{ params[ :controller ].gsub( '_', '-' ) }"
+    css_class = "#{ params[ :action ].gsub( '_', '-' ) } #{ params[ :controller ].gsub( '_', '-' ) }"
+    if @item.present?
+      css_class += " show--#{ @item.slug }"
+    end
+    css_class
   end
 
   def social_urls
