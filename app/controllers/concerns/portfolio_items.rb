565ec915d2bf2ea353d9b7817a23fac2295343d8
Angela Potter <angelacbpotter@gmail.com>
2017-08-20 12:43:28 -0700

    Update styles and resume

--- app/controllers/concerns/portfolio_items.rb
+++ app/controllers/concerns/portfolio_items.rb
@@ -9,7 +9,7 @@ class PortfolioItems < ActiveHash::Base
       role: 'Design',
       platform: 'Web & Mobile',
       slug: 'doubledutch-unified-messaging',
-      blurb: "A messaging center for attendees to see all their chat activity in one place.",
+      blurb: "A messaging center for event attendees to see all their chat activity in one place.",
       # show_case_study: true,
     },
     {
