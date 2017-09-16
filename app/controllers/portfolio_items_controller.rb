3e8eec2f321f83e7338296d0419b945dacb0fc54
Angela Potter <angelacbpotter@gmail.com>
2017-09-16 16:21:51 -0700

    Move outbound links and fix project nav

--- app/controllers/portfolio_items_controller.rb
+++ app/controllers/portfolio_items_controller.rb
@@ -1,5 +1,10 @@
 class PortfolioItemsController < ApplicationController
 
+  helper_method :prev_portfolio_item
+  helper_method :next_portfolio_item
+  helper_method :prev_portfolio_item_path
+  helper_method :next_portfolio_item_path
+
   def show
     portfolio_items.each_with_index do |item, index|
       if item.slug == params[ :slug ]
@@ -10,6 +15,30 @@ class PortfolioItemsController < ApplicationController
     end
 
     redirect_to work_path
+  end
+
+  def prev_portfolio_item
+    @portfolio_items[ @index - 1 ]
+  end
+
+  def next_portfolio_item
+    @portfolio_items[ @index + 1 ]
+  end
+
+  def prev_portfolio_item_path
+    if prev_portfolio_item.present?
+      portfolio_items_path( prev_portfolio_item.slug )
+    else
+      '#'
+    end
+  end
+
+  def next_portfolio_item_path
+    if next_portfolio_item.present?
+      portfolio_items_path( next_portfolio_item.slug )
+    else
+      '#'
+    end
   end
 
 end
